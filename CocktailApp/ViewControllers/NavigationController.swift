10c652fe2a9a4de886ca4724b416cfcb21bc5a1c
kiiich <59602632+kiiich@users.noreply.github.com>
2021-09-26 21:23:13 +0300

    Block autorotate

---
+++ CocktailApp/ViewControllers/NavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationController.swift
+//  CocktailApp
+//
+//  Created by Николай on 26.09.2021.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override var shouldAutorotate: Bool {
+        false
+    }
+
+}
